2a06875ff6ab2cc739fd73fd7e66c2c8b2b3f56f
Iliyan <eltraficante000@gmail.com>
2024-07-01 14:49:45 +0300

    Added new challenge solutions

---
+++ 8kyu/Abbreviate a Two Word Name.js
@@ -0,0 +1,19 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+// My solution: 
+
+function abbrevName(name){
+
+    let nameArray = name.split(' ');
+  let initials = nameArray[0][0].toUpperCase() + '.' + nameArray[1][0].toUpperCase();
+  return initials;
+
+}
---
+++ 8kyu/How good are you really.js
@@ -0,0 +1,21 @@
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+
+// Note:
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+// My solution 
+
+function betterThanAverage(classPoints, yourPoints) {
+  
+    let totalPoints = classPoints.reduce((sum, point) => sum + point, 0);
+    
+    let averagePoints = totalPoints / classPoints.length;
+ 
+    return yourPoints > averagePoints;  
+}

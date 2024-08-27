6c8215ca678d8c183f98478c9819a31a0f1e0200
Iliyan <eltraficante000@gmail.com>
2024-08-27 16:44:07 +0300

    Added new challenge solution

--- 8kyu/Opposites Attract.js
+++ 8kyu/Opposites Attract.js
@@ -3,6 +3,8 @@
 
 // Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
 
+// My solution
+
 function lovefunc(flower1, flower2) {
 
     const isFlower1Even = flower1 % 2 === 0;
